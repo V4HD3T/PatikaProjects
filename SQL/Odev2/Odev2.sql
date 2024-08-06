cbf0f772816a435be126ce9421b0baffa3e7ce81
Vahdet Eren BOZYİL <vahdeterenbozyil@gmail.com>
2024-08-06 12:10:20 +0300

    SQL_Odev2

--- SQL/Odev2/Odev2.sql
+++ SQL/Odev2/Odev2.sql
@@ -1,24 +1,14 @@
--- 1) film tablosunda bulunan title ve description sütunlarýndaki verileri sýralayýnýz.
-SELECT title, description
-FROM film;
-
--- 2) film tablosunda bulunan tüm sütunlardaki verileri film uzunluðu (length) 60 dan büyük VE 75 ten küçük olma koþullarýyla sýralayýnýz.
-SELECT * 
-FROM film
-WHERE length > 60 AND length < 75;
-
--- 3) film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99 VE replacement_cost 12.99 VEYA 28.99 olma koþullarýyla sýralayýnýz.
-SELECT * 
-FROM film
-WHERE rental_rate = 0.99 AND (replacment_cost = 12.99 OR replacment_cost = 28.99);
-
--- 4) customer tablosunda bulunan first_name sütunundaki deðeri 'Mary' olan müþterinin last_name sütunundaki deðeri nedir?
-SELECT last_name
-FROM customer
-WHERE first_name = 'Mary';
-
-
--- 5) film tablosundaki uzunluðu(length) 50 ten büyük OLMAYIP ayný zamanda rental_rate deðeri 2.99 veya 4.99 OLMAYAN verileri sýralayýnýz.
+-- 1) film tablosunda bulunan tüm sütunlardaki verileri replacement cost deðeri 12.99 dan büyük eþit ve 16.99 küçük olma koþuluyla sýralayýnýz ( BETWEEN - AND yapýsýný kullanýnýz.)
 SELECT *
 FROM film
-WHERE NOT length > 50 AND NOT (rental_rate = 2.99 OR rental_rate = 4.99);
+WHERE replacement cost BETWEEN 12.99 AND 16.99;
+
+-- 2) .actor tablosunda bulunan first_name ve last_name sütunlardaki verileri first_name 'Penelope' veya 'Nick' veya 'Ed' deðerleri olmasý koþuluyla sýralayýnýz. ( IN operatörünü kullanýnýz.)
+SELECT first_name, last_name
+FROM actor
+WHERE first_name IN ('Penelope', 'Nick', 'Ed');
+
+-- 3) film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99, 2.99, 4.99 VE replacement_cost 12.99, 15.99, 28.99 olma koþullarýyla sýralayýnýz. ( IN operatörünü kullanýnýz.)s
+SELECT * 
+FROM film
+WHERE rental_rate IN (0.99, 2.99, 4.99) AND replacement_cost IN (12.99, 15.99, 28.99);
